b21326d1089e216d55e04ba328abd53ece4a7323
Joao Otavio Manieri <jotamanieri@ufu.br>
2022-11-03 18:03:21 -0300

    att task4

--- trabalho4/ex5/script.js
+++ trabalho4/ex5/script.js
@@ -1,4 +1,19 @@
 window.onload = function () {
     buttons = document.querySelectorAll("nav button");
-    
+    for (let button of buttons) {
+        button.onclick = () => openTab(button.dataset.tabname);
+    }
+    openTab("BCC");
+}
+
+function openTab(tabName) {
+    const lastTabActive = document.querySelector(".tabActive");
+    if (lastTabActive !== null)
+    lastTabActive.className = "";
+
+    const query1 = ".tabs > section[data-tabname = '" + tabName + "']"; 
+    const query2 = "nav button[data-tabname='" + tabName + "']";
+
+    document.querySelector(query1).className="tabActive";
+    document.querySelector(query2).className="buttonActive";
 }
