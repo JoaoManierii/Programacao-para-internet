236c00d5a2f116bf818e63659385e5dc2cc96409
Joao Otavio Manieri <jotamanieri@ufu.br>
2022-12-13 15:21:19 -0300

    add ex01

---
+++ trabalho6/ex1/script.php
@@ -0,0 +1,8 @@
+<?php
+$CEP = $_POST["CEP"];
+$Logadouro = $_POST["Logadouro"];
+$Bairro = $_POST["Bairro"];
+$Cidade = $_POST["Cidade"];
+$Estado = $_POST["Estado"];
+echo "Sr. seu CEP é $CEP, seu logadouro é $Logadouro, seu bairro é $Bairro, voce mora na cidade de $Cidade, cujo estado pertencente é $Estado";
+?>
