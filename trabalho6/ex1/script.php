209687b15f6548ea8fb4dcb8dfe1b92266412be1
Joao Otavio Manieri <jotamanieri@ufu.br>
2022-12-13 23:19:13 -0300

    att

--- trabalho6/ex1/script.php
+++ trabalho6/ex1/script.php
@@ -1,8 +1,30 @@
-<?php
-$CEP = $_POST["CEP"];
-$Logadouro = $_POST["Logadouro"];
-$Bairro = $_POST["Bairro"];
-$Cidade = $_POST["Cidade"];
-$Estado = $_POST["Estado"];
-echo "Sr. seu CEP é $CEP, seu logadouro é $Logadouro, seu bairro é $Bairro, voce mora na cidade de $Cidade, cujo estado pertencente é $Estado";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+   	<!-- 1: Tag de responsividade -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Página Dinâmica</title>
+    
+    <!-- 2: Bootstrap CSS -->
+   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">-->
+  </head>
+<body>
+		<?php
+        $CEP = $_POST["CEP"];
+        $Logadouro = $_POST["Logadouro"];	
+        $Bairro = $_POST["Bairro"];
+        $Cidade = $_POST["Cidade"];
+        echo <<<HTML 
+    <table>
+      <tr>
+        <td>$CEP</th>
+        <td>$Logadouro</th>
+        <td>$Bairro</th>
+        <td>$Cidade</th>
+      </tr>
+    </table>
+    HTML;
+		?>
+</body>
+</html>
