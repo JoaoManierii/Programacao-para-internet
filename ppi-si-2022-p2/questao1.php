7840290971f11e451188cde0329d6b54db278fef
Joao Otavio Manieri <jotamanieri@ufu.br>
2023-01-21 09:30:23 -0300

    add prova

---
+++ ppi-si-2022-p2/questao1.php
@@ -0,0 +1,52 @@
+<?php
+
+	require "conexaoMysql.php";
+	$pdo = mysqlConnect();
+
+	$nome = $_POST["func_nome"] ?? "";
+	$cpf = $_POST["func_cpf"] ?? "";
+	$email = $_POST["func_email"] ?? "";
+
+	$admissao = $_POST["func_admissao"] ?? "";
+	$salario = $_POST["func_salario"] ?? "";
+
+	$nome = htmlspecialchars($nome);
+	$cpf = htmlspecialchars($cpf);
+	$email = htmlspecialchars($email);
+	$admissao = htmlspecialchars($admissao);
+	$salario = htmlspecialchars($salario);
+
+	$sqlpessoa = <<<SQL
+		INSERT INTO pessoa(nome, cpf, email)
+		VALUES(?, ?, ?)
+	SQL;
+
+	$sqlfuncionario = <<<SQL
+		INSERT INTO funcionario(data_admissao, salario, cod_pessoa)
+		VALUES(?, ?, ?)
+	SQL;
+
+	try{
+
+		$pdo->beginTransaction();
+
+		$stmt = $pdo->prepare($sqlpessoa);
+		if(!$stmt->execute([$nome, $cpf, $email]))
+			throw new Exception('Falha ao cadastrar pessoa');
+
+		$cod_pessoa= $pdo->lastInsertId();
+
+		$stmt = $pdo->prepare($sqlfuncionario);
+        if(!$stmt->execute([$admissao, $salario, $cod_pessoa]))
+            throw new Exception('Falha ao cadastrar funcionario');
+
+		$pdo->commit();
+		header("location: questao1.html");
+        exit();
+
+	}
+	catch (Exception $e){
+        $pdo->rollBack();
+        exit('Falha inesperada: ' . $e->getMessage());
+	}
+?>
---
+++ ppi-si-2022-p2/questao2.php
@@ -0,0 +1,78 @@
+<?php
+
+require "conexaoMysql.php";
+$pdo = mysqlConnect();
+
+    $salMin = $_GET['salMin'] ?? '';
+
+    $salMin = htmlspecialchars($salMin);
+
+    if($salMin == null)
+        $salMin = 0;
+
+    function buscaSalario($pdo, $salMin)
+    {
+        $sql = <<<SQL
+            SELECT nome,email,salario
+            FROM pessoa, funcionario
+            WHERE pessoa.codigo = funcionario.cod_pessoa AND funcionario.salario > ?
+        SQL;
+    try {
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$salMin]);
+        $row = $stmt->fetch();
+        if (!$row) return "";
+        return $row;
+  } 
+  catch (Exception $e) {
+    exit('Falha: ' . $e->getMessage());
+  }
+}
+
+$funcionarios = buscaSalario($pdo, $salMin);
+
+header('Content-type: application/json');
+
+echo json_encode($funcionarios);
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="UTF-8">
+		<title>Questao 2</title>
+	</head>
+	<body>
+		<table>
+			<thead>
+				<tr>
+					<th>Nome</th>
+					<th>cpf</th>
+					<th>email</th>
+					<th>admissao</th>
+					<th>salario</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+					while($row = $stmt->fetch()){
+                        $nome = htmlspecialchars($nome);
+                        $cpf = htmlspecialchars($cpf);
+                        $email = htmlspecialchars($email);
+                        $admissao = htmlspecialchars($admissao);
+                        $salario = htmlspecialchars($salario);
+						echo <<<HTML
+							<tr>
+								<td>$nome</td>
+								<td>$cpf</td>
+								<td>$email</td>
+								<td>$admissao</td>
+								<td>$salario</td>
+							</tr>
+						HTML;
+					}
+				?>
+			</tbody>
+		</table>
+	</body>
+</html>
+
